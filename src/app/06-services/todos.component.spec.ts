996c65c90458738d15e284a9a7b70c072cfb2b30
Jelani <eyeglass0324@gmail.com>
2022-05-23 17:21:06 -0400

    todo add testing completed

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,6 +1,6 @@
 import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
-import { from, of } from 'rxjs';
+import { EMPTY, from, of, throwError } from 'rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -24,5 +24,35 @@ describe('TodosComponent', () => {
     component.ngOnInit();
 
     expect(component.todos).toBe(todos);
+  });
+
+  it('should call the server to save the changes when a new todo item is added', () => {
+    let spy = spyOn(service, 'add').and.callFake(() => {
+      return EMPTY;
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo returned from the server', () => {
+    let todo = { id: 1 };
+    spyOn(service, 'add').and.callFake(() => {
+      return from([todo]);
+    });
+
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+
+  it('should set the message property if server returns an error when adding a new todo', () => {
+    let error = 'error from the server';
+    spyOn(service, 'add').and.returnValue(from(throwError(() => error)));
+
+    component.add();
+
+    expect(component.message).toBe(error);
   });
 });
